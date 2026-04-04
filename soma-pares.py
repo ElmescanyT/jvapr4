9efa0c43315b5fe74c3aec53fd52de14adf9866f
ElmescanyT <trigoelmescany@gmail.com>
2026-04-04 14:47:49 -0300

    Delete soma-pares.py

--- soma-pares.py
+++
@@ -1,8 +0,0 @@
-soma = 0
-cont = 0
-for p in range(1, 7):
-    n = int(input('Digite o {} número: '.format(p)))
-    if n % 2 == 0:
-        soma += n
-        cont += 1
-print('A soma entre {} número(s) pares foi {}.'.format(cont, soma))
